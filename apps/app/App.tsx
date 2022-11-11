8044ab6f98858201f3315afd3c9821b59c3cb236
David Smith <david@ultimatetag.com>
2022-11-11 09:32:02 -0800

    cleanup app

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -1,16 +1,7 @@
 import { test, UserProvider } from "client-sdk";
 import { StyleSheet } from "react-native";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { RootStackParamList } from "./RootStackParams";
-
-import CameraScreen from "./Camera";
-import HomeScreen from "./Home";
-import RegCameraScreen from "./RegCamera";
 import Navigation from "./Navigation";
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   const message = test();
