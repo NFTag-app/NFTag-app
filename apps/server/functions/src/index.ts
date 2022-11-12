2d68065780407517eb55888ec080c09c97ce871a
Walter Shewmake <25shewmwalj@washk12.org>
2022-11-12 07:21:09 -0700

    server

---
+++ apps/server/functions/src/index.ts
@@ -0,0 +1,16 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.createNFT = functions.database
+  .ref(`/games/{gameId}/tags/{tagId}`)
+  .onCreate((snapshot, context) => {
+    const data = snapshot.val();
+    const base64 = data.image.uri;
+  });
