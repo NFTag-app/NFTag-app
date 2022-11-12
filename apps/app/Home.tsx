c9c9971f64a0098e05c4bf8b6c4d93b1a9879257
Spencer Smith <spencerliamsmith@gmail.com>
2022-11-12 00:55:10 -0700

    home

--- apps/app/Home.tsx
+++ apps/app/Home.tsx
@@ -1,7 +1,7 @@
-import { View, Text, LayoutChangeEvent } from "react-native";
+import { View, Text, LayoutChangeEvent, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { HomeButton } from "./components/buttons/HomeButton";
-import { useUser } from "client-sdk";
+import { signOut, useUser } from "client-sdk";
 import { LogoutButton } from "./components/buttons/LogoutButton";
 import { LoginButton } from "./components/buttons/LoginButton";
 import { UserData } from "client-sdk/dist/types";
@@ -11,18 +11,12 @@ import { GoogleSignIn } from "client-sdk";
 export const HomeScreen = () => {
   const user: UserData = useUser();
 
-  if(user) {
+  if (!user) {
     return (
       <View style={CommonStyles.container}>
-
         <Text style={CommonStyles.text}>NFTag</Text>
 
-        <GoogleSignIn/>
-        {/* <LoginButton user={user} /> */}
-
-        <HomeButton caption="Game List" navigateTo="GameListScreen" />
-
-        <LogoutButton />
+        <GoogleSignIn />
 
         <StatusBar style="auto" />
       </View>
@@ -30,18 +24,26 @@ export const HomeScreen = () => {
   }
   return (
     <View style={CommonStyles.container}>
-
       <Text style={CommonStyles.text}>Welcome</Text>
 
       <Text style={CommonStyles.text}>{user && user.displayName}</Text>
 
       <HomeButton caption="Game List" navigateTo="GameListScreen" />
 
-      <LogoutButton />
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#47f",
+          width: 150,
+          padding: 10,
+          margin: 10,
+          alignItems: "center",
+        }}
+        onPress={() => signOut()}
+      >
+        <Text style={{ fontSize: 18, color: "#fff" }}>Logout</Text>
+      </TouchableOpacity>
 
       <StatusBar style="auto" />
     </View>
-  )
-  
+  );
 };
-
