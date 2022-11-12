aa6d344f663ed143c607d3fc0d561ffeebceebb1
Spencer Smith <spencerliamsmith@gmail.com>
2022-11-11 17:32:34 -0700

    Update GameListScreen.tsx

--- apps/app/GameListScreen.tsx
+++ apps/app/GameListScreen.tsx
@@ -1,15 +1,23 @@
-import { FlatList, View, Text, StyleSheet } from "react-native";
+import { FlatList, View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Game } from "client-sdk/dist/types";
 import { useGames } from "client-sdk";
+import { CommonStyles } from "./styles/CommonStyles";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "./RootStackParams";
 
+type InGameScreenProps = NativeStackNavigationProp<RootStackParamList, "InGameScreen">;
 export const GameListScreen = () => {
     const games: Game[] = useGames();
+    const navigation = useNavigation<InGameScreenProps>()
 
     const renderItem = ({ item, index, separators }) => (
-        <View style={{height: 200}}>
-        <Text>
-            Key: {item.id} Name: {item.name}
-        </Text>
+        <View>
+        <TouchableOpacity onPress={() => navigation.navigate(item.id)} style={{backgroundColor: 'green', padding: 20}}>
+            <Text style={CommonStyles.text}>
+                Key: {item.id} Name: {item.name}
+            </Text>
+        </TouchableOpacity>
         </View>
     );
 
@@ -25,6 +33,5 @@ export const GameListScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
   },
 });
