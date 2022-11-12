2ee6aeb9b80ea93e91652d62642d90113db8ad6c
Spencer Smith <spencerliamsmith@gmail.com>
2022-11-12 04:57:53 -0700

    fix

--- apps/app/JoinGameScreen.tsx
+++ apps/app/JoinGameScreen.tsx
@@ -1,10 +1,10 @@
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { CameraType } from "expo-camera";
 import { cameraStyles, NftagCamera } from "./components/camera/NftagCamera";
 import { InGameStackParamList, RootStackParamList } from "./RootStackParams";
 import { joinGame, useUser } from "client-sdk";
 import { UserData } from "client-sdk/dist/types";
-import { useState } from "react";
+import React, { useState } from "react";
 //import { renderBlankOverlay } from "./components/camera/Overlay";
 
 const JoinGameScreen = ({navigation: {navigate}}) => {
@@ -14,43 +14,45 @@ const JoinGameScreen = ({navigation: {navigate}}) => {
 
   const renderOverlay = ({ screenSize, vertPadding, cameraDetails }) => {
     return (
-      <View style={cameraStyles.overlay}>
-        <View
-          style={{
-            alignItems: "center",
-            marginTop: screenSize.height / 6,
-          }}
-        >
-          <Text
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={cameraStyles.overlay}>
+          <View
             style={{
-              color: "white",
-              marginBottom: 10,
-              fontSize: Math.floor(screenSize.width / 16),
+              alignItems: "center",
+              marginTop: screenSize.height / 6,
             }}
           >
-            Game Code
-          </Text>
-          <TextInput
-            keyboardType="numeric"
-            maxLength={6}
-            autoCapitalize="characters"
-            placeholderTextColor={"#FFFFFF88"}
-            style={{
-              textAlign: "center",
-              fontSize: Math.floor(screenSize.width / 10),
-              height: 80,
-              width: screenSize.width / 1.6,
-              borderWidth: 2,
-              borderColor: "white",
-              padding: 10,
-              color: "white",
-            }}
-            onChangeText={setGameId}
-            value={gameId}
-            placeholder="123456"
-          />
+            <Text
+              style={{
+                color: "white",
+                marginBottom: 10,
+                fontSize: Math.floor(screenSize.width / 16),
+              }}
+            >
+              Game Code
+            </Text>
+            <TextInput
+              keyboardType="numeric"
+              maxLength={6}
+              autoCapitalize="characters"
+              placeholderTextColor={"#FFFFFF88"}
+              style={{
+                textAlign: "center",
+                fontSize: Math.floor(screenSize.width / 10),
+                height: 80,
+                width: screenSize.width / 1.6,
+                borderWidth: 2,
+                borderColor: "white",
+                padding: 10,
+                color: "white",
+              }}
+              onChangeText={setGameId}
+              value={gameId}
+              placeholder="123456"
+            />
+          </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     );
   };
 
