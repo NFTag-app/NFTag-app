ba3ca6f06031d2f4b1953800bb7951521f948361
Spencer Smith <spencerliamsmith@gmail.com>
2022-11-12 01:13:35 -0700

    home

--- apps/app/RootStackParams.tsx
+++ apps/app/RootStackParams.tsx
@@ -1,5 +1,6 @@
 export type RootStackParamList = {
   Home: undefined;
+  JoinGameScreen: undefined;
   RegCamera: undefined;
   GameListScreen: undefined;
   GameNavigatorScreen: { gameId: string } | undefined;
